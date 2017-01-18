00195d709f3ffd51857ab2b52be2681450b48886
Serhiy Yanchuk <yanchukserhiy96@gmail.com>
2017-01-18 22:55:26 +0200

    responsive home page

---
+++ js/animated.js
@@ -0,0 +1,66 @@
+
+
+// aniamte secondtext
+$(document).ready(function () {
+        var objToStick = $(".box"); //Получаем нужный объект
+        var topOfObjToStick = $(objToStick).offset().top; //Получаем начальное расположение нашего блока
+
+        $(window).scroll(function () {
+            var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно
+
+            if (windowScroll = topOfObjToStick) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+                $(objToStick).addClass('animated slideInUp');
+           
+            };
+        });
+    });
+
+
+
+
+
+// плавний скролінг
+$(document).ready(function(){
+    
+    setBindings(); 
+    
+});
+
+function setBindings() {
+    
+    $("li a").click(function(e){
+        
+        e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+        
+       
+        
+        $("html body").animate({
+            
+            scrollTop: $("#" + sectionID).offset().top
+        }, 1000)
+    })
+    
+}
+		 
+
+
+
+//animation for search input
+$(document).ready(function()
+ {
+ var flag=true;
+ $("#search").click(function()
+  {
+  if(flag==true)
+  {
+    $(".inputCss").show("slow");
+    flag=!flag;
+  }
+  else
+  {
+   $(".inputCss").hide("slow");
+
+                flag=!flag;}
+    });
+});
---
+++ js/nav.js
@@ -0,0 +1,35 @@
+ // адаптивное меню
+$(document).ready(function() {
+  $('#menu-trigger').click(function() {
+    $('.nav').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {		
+		if (  $(window).width() > 500 ) {			
+			$('.nav').removeAttr('style');
+		 }
+	});//end resize
+});//end ready
+
+
+
+
+
+//  OPEN AND CLOSE NAVBAR
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    
+    document.getElementById("myBd").style.padding = "5%";
+
+ 
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myBd").style.padding = "0";
+     
+}
